a277f1e112b217d1146c7765de281cdee921b532
fuad_suleymanov <fuadsuleymanov@outlook.com>
2020-04-07 10:52:54 +0400

    use redux for state

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,53 +1,74 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import Cardlist from '../components/Cardlist';
 import Search from '../components/Search';
 import Scroll from '../components/Scroll';
 import ErrorBoundry from '../components/ErrorBoundry';
 import './App.css';
+import { setSearchField, requestRobots } from '../actions';
 
 
+const mapStateToProps = state => {
+    return {
+        searchField: state.searchRobots.searchField,
+        robots: state.requestRobots.robots,
+        isPending: state.requestRobots.isPending,
+        error: state.requestRobots.error
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+        onRequestRobots: () => dispatch(requestRobots())
+    }
+}
 
 class App extends Component {
 
     // asagida 2 state-miz var robots ve searchfields
     // state- ancaq class componentde yaratmaq olur
     // state props-dan ferqli olaraq deyisile bilir
-    constructor() {
-        super()
-        this.state = {
-            robots: [],
-            searchfields: ''
-        }
-    }
+    // redux istifade etdikden sonra construktora ehtiyac qalmadi stat-ler redux-da
+    // constructor() {
+    //     super()
+    //     this.state = {
+    //         robots: []
+    //     }
+        // redux istidade edenden sonra yuxarida field-den sildim searchField: ''
+    
 
-    // constructor-dan sonra asagidaki yuklenir linkden melumatlar goturur , robotsa verir
+    // constructor-dan sonra asagidaki yuklenir linkden melumatlar goturur, robotsa verir
     componentDidMount(){
-        fetch('https://jsonplaceholder.typicode.com/users')
-        .then(response => response.json())
-        .then(users => this.setState({robots: users}));
+        // asagidakini yoxlamaq ucun qoydum, hazirki statusu gosterir
+        //console.log(this.props.store.getState());
+        // fetch('https://jsonplaceholder.typicode.com/users')
+        // .then(response => response.json())
+        // .then(users => this.setState({robots: users}));
+        // yuxaridaki 3 serte redux gelenden sonra ehtiyac qalmadi
+        this.props.onRequestRobots(); 
     }
 
     // search hissesine yazilanlari tutur, asagida search komponentinin icine bax
-    onSearchChange = (event) => {
-        this.setState({ searchfields: event.target.value })
-    }
+    // redux istifade edenden sonra asagidakina ehtiyac qalmadi
+    // onSearchChange = (event) => {
+    //     this.setState({ searchfields: event.target.value })
+    // }
 
     render(){
         // destructisation etdim ve heryerde this.state.robots ve ya this.state.searchfields yazmaga ehtiyac qalmadi 
-        const {robots, searchfields} = this.state;
-
-        
+        const {searchField, onSearchChange, robots, isPending} = this.props;
         const filteredRobots = robots.filter( robot => {
-            return robot.name.toLocaleLowerCase().includes(searchfields.toLocaleLowerCase());
+            return robot.name.toLowerCase().includes(searchField.toLowerCase());
         })
         // asagidaki if le deyrikki yuxaridaki linkden melumat gec gelse, ve ya linke problem olsa ekrana bu cixsin
-        if (!robots.length) {
+        if (isPending) {
             return <h1 className="tc">Loading</h1>
         } else {
             return (
                 <div className="tc">
                     <h1 className="f2">Robo Friends</h1>
-                    <Search className="bg-light-green" searchChange={this.onSearchChange}/>
+                    <Search className="bg-light-green" searchChange={onSearchChange}/>
                     {/* scroll elave etdim */}
                     
                     <ErrorBoundry>
@@ -62,5 +83,6 @@ class App extends Component {
     }
 }
 
-export default App;
+
+export default connect(mapStateToProps, mapDispatchToProps) (App);
 
--- src/index.js
+++ src/index.js
@@ -1,16 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { createLogger } from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
 import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
+import { searchRobots, requestRobots } from './reducers';
+
+// kodla ishlemeyi asanlasdiri consola infolar cixarir
+const logger = createLogger();
+
+const rootReducer = combineReducers({ searchRobots, requestRobots })
+
+// redux
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, logger));
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     
       <App/>
     
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
